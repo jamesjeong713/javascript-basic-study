1a9d0f1546201d8b2b25fdb31538268adcc99604
Seongyeop Jeong <jeon0042@algonquinlive.com>
2019-05-19 23:56:38 -0400

    organize folder and added studies

---
+++ 010_CallBack/function_01.js
@@ -0,0 +1,13 @@
+// After the return, you do not have to put SEMICOLON!
+
+function cal(func, num) {
+    return func(num)
+}
+
+function increase(num) {
+    return num+1
+}
+
+function decrease(num) {
+    return num-1
+}
